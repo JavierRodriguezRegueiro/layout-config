4148dd24d08c080beb7cff3db12f26da36bdfd77
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-11-13 16:16:23 +0100

    Include storybook dependecy

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/app/ui/**/*.stories.mdx",
+    "../src/app/ui/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
