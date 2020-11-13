216cfbeee5ba03aba57644dbb45b190f60a62415
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-11-13 16:08:01 +0100

    Replace LayoutItemCreator to UserCase in injectable configuration

--- src/app/confg/dependency-injection/inversify.config.ts
+++ src/app/confg/dependency-injection/inversify.config.ts
@@ -1,12 +1,13 @@
 import {Container} from "inversify";
 import {TYPES} from "../../../core/types";
 import "reflect-metadata";
+import UserCase from "../../../core/Shared/Domain/UserCase/UserCase";
 import LayoutItemRepository from "../../../core/LayoutItem/Domain/LayoutItemRepository";
 import LocalStorageLayoutItemRepository from "../../../core/LayoutItem/Infrastructure/LocalStorageLayoutItemRepository";
 import LayoutItemCreator from "../../../core/LayoutItem/Application/LayoutItemCreator";
 
 const container = new Container()
-container.bind<LayoutItemCreator>(TYPES.LAYOUT_ITEM_CREATOR).to(LayoutItemCreator);
+container.bind<UserCase>(TYPES.LAYOUT_ITEM_CREATOR).to(LayoutItemCreator);
 container.bind<LayoutItemRepository>(TYPES.LAYOUT_ITEM_REPOSITORY).to(LocalStorageLayoutItemRepository);
 
 export {container}
--- src/core/Shared/Domain/UserCase/UserCase.ts
+++ src/core/Shared/Domain/UserCase/UserCase.ts
@@ -1,3 +1,4 @@
 export default interface UserCase {
+    run(...args : any[]): Promise<any>;
     getRepository(): any;
 }
